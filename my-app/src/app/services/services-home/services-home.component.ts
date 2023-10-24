db09bb72528aa2fb3db10173d8a11499364f9121
Dealer03 <crazydealer2003@protonmail.com>
2023-10-23 21:41:03 -0400

    Adds a structural directive to home-services

--- my-app/src/app/services/services-home/services-home.component.ts
+++ my-app/src/app/services/services-home/services-home.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./services-home.component.css']
 })
 export class ServicesHomeComponent {
-
+  bankingServices: any[] = [
+    {
+      "type": 'Issue business loans',
+      "years": 2,
+      "category": "loans"
+    },
+    {
+      "type": 'Provide periodic reports and analysis',
+      "years": 7,
+      "category": "reports"
+    },
+    {
+      "type": 'Open new checking and/or saving accounts',
+      "years": 5,
+      "category": "financial_accounts"
+    },
+    {
+      "type": 'Guide clients through their savings, loan, mortgage, and retirement options',
+      "years": 6,
+      "category":"options"
+    }
+  ];
 }
